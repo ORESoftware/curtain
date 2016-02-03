dbb83a8551792f8b2868acb0944cd536bd827354
the1mills <alexander.d.mills@gmail.com>
2016-02-03 01:15:39 -0800

    FC4

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
 var Redis = require('ioredis');
 var ijson = require('idempotent-json');
 
-function Rate(conf){
+function Rate(conf) {
 
     this.client = new Redis(conf.redis);
 
@@ -18,38 +18,55 @@ function Rate(conf){
 }
 
 
-
-Rate.prototype.rateLimit = function rateLimit(opts){
+Rate.prototype.rateLimit = function rateLimit(opts) {
 
     var self = this;
 
-    return function(req,res,next){
+    var maxRequests = self.maxRequests || 50;
+    var maxRequestsTime = self.maxRequestsTime || 1000;
+
+    return function (req, res, next) {
 
         var key = req.ip;
 
-        if(!key){
+        if (!key) {
             next();
         }
-        else{
+        else {
 
-            var now = Date.now();
+            self.client.get(key, function (err, result) {
 
-            self.client.get(key, function(err,result){
-
-                if(err){
+                if (err) {
                     next(err);
                 }
-                else if(result){
+                else if (result) {
 
-                    result = ijson.parse(result);
 
-                    result.push(now);
+                    result = ijson.parse(result).sort(function (a, b) {
+                        return a - b;
+                    });
+
+                    var now = Date.now();
+                    result.push(now); //push timestamp of latest request
+
+                    var length = result.length;
+
+                    var old, error = null;
+
+                    if (length >= maxRequests) {
+                        old = result.shift();  // get the oldest request time and examine it
+                        if (now - old <= maxRequestsTime) {
+                            error = {error: 'Exceeded 50 requests per second for XRE events'};
+                        }
+                    }
 
                     self.client.set(key, result);
+                    next(error);
 
                 }
-                else{
-
+                else {
+                    self.client.set(key, [Date.now()]);
+                    next();
                 }
 
             });
@@ -59,11 +76,6 @@ Rate.prototype.rateLimit = function rateLimit(opts){
 
 
 };
-
-
-
-
-
 
 
 module.exports = Rate;
