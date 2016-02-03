04b8886e64e5406b9d9f4cdda125c8c9aa103065
the1mills <alexander.d.mills@gmail.com>
2016-02-03 00:50:25 -0800

    FC2

--- index.js
+++ index.js
@@ -1,3 +1,69 @@
 /**
  * Created by denman on 2/3/2016.
  */
+/**
+ * Created by denman on 2/3/2016.
+ */
+
+
+
+var Redis = require('ioredis');
+var ijson = require('idempotent-json');
+
+function Rate(conf){
+
+    this.client = new Redis(conf.redis);
+
+
+}
+
+
+
+Rate.prototype.rateLimit = function rateLimit(opts){
+
+    var self = this;
+
+    return function(req,res,next){
+
+        var key = req.ip;
+
+        if(!key){
+            next();
+        }
+        else{
+
+            var now = Date.now();
+
+            self.client.get(key, function(err,result){
+
+                if(err){
+                    next(err);
+                }
+                else if(result){
+
+                    result = ijson.parse(result);
+
+                    result.push(now);
+
+                    self.client.set(key, result);
+
+                }
+                else{
+
+                }
+
+            });
+        }
+
+    }
+
+
+};
+
+
+
+
+
+
+
+module.exports = Rate;
