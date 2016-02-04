458bb1126f078b995ca95c2bf545294976726883
Alex Mills <alexmillsmail@gmail.com>
2016-02-04 13:13:18 -0800

    Added code to generify the key/identifier

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ a request has exceeed a threshold and should be rejected with a HTTP 429 or othe
 ## Usage
 
 
+```javascript
 
 const RateLimiter = require('curtain');
 
@@ -20,18 +21,76 @@ var rlm = new RateLimiter({
 });
 
 
+app.use(rlm.limit({
 
+    maxReqsPerPeriod: 150,          // maximum number of requests that are allowed to occur during a window
+    periodMillis: 3000,             // the window period in milliseconds
+    identifier: 'ip'                // string representing what value to read off the req object
+    
+
+}), function requestLimitExceeded(err, req, res, next) {
+
+    //some error occured, most likely an error representing that the request rate was exceeded by the latest request
+    next(err);
+
+}, function requestIsOk(req, res, next) {
+
+    //the new request is within limits
+    next();
+});
+
+```
+
+
+you can also pass in an existing Redis client like so:
+
+
+```javascript
+const RateLimiter = require('curtain');
+
+var rlm = new RateLimiter({
+    redis: {
+        client: yourClient 
+    }
+});
+```
+
+Note: This library calls your error handling middleware. This library will not throw any errors. When the rate is exceeded by the request 
+it will call your next error handling middleware; this same middleware will also be called if any other types of errors occur. 
+All errors (whether they are Redis errors or rate limit errors) should be handled by you like this:
+
+```javascript
 app.use(rlm.limit({
 
     maxReqsPerPeriod: 150,
-    periodMillis: 3000
+    periodMillis: 3000,
+    identifier: 'ip'
 
 }), function limitExceeded(err, req, res, next) {
 
-    log.debug({msg: 'request was rejected because it exceeded the rate limit'});
-    next(err);
+        switch (err.type) {   //Express allows you to pass an object as first argument, not always an instanceof Error
+            case rlm.errors.RATE_EXCEEDED:
+                next(err);
+                break;
+            case rlm.errors.REDIS_ERROR:
+                next(err);
+                break;
+            case rlm.errors.NO_KEY:  // whatever you chose to use as your request unique identifier, there was a problem finding it on the request stream object
+                next(err);
+                break;
+            case rlm.errors.BAD_ARGUMENTS:  //if you have some crazy dynamicism in your project, then it's possible that you could pass bad args at runtime
+                next(err);
+                break;
+            default:
+                next(new Error('The NPM curtain library broke because it sent an unexpected error, what a POS'))
+        }
 
 }, function requestIsOk(req, res, next) {
 
     next();
 });
+```
+
+
+If you don't use the ip value of req.ip, (which you probably shouldn't) then you need to attach a value to req representing 
+the key to use for that user that is making the request.
--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function Rate(conf) {
 }
 
 
-Rate.errors = Object.freeze({
+Rate.errors = Rate.prototype.errors = Object.freeze({  //TODO Object.freeze may prevent syntax highlighting..
 
     'NO_KEY': 'NO_KEY',
     'RATE_EXCEEDED': 'RATE_EXCEEDED',
@@ -34,27 +34,28 @@ Rate.prototype.limit = function rateLimit(opts) {
 
     var maxReqsPerPeriod = opts.maxReqsPerPeriod ? parseInt(opts.maxReqsPerPeriod) : null;
     var periodMillis = opts.periodMillis ? parseInt(opts.periodMillis) : null;
+    var identifier = opts.identifier ? String(opts.identifier) : null;
 
     return (req, res, next) => {
 
-        var key = String(req.ip);
+        var key = String(req[identifier]);
 
         debug('ip address:', key);
 
-        if (!maxReqsPerPeriod || !periodMillis) {
+        if (!maxReqsPerPeriod || !periodMillis || !identifier) {
             next({
-                error: Rate.errors.BAD_ARGUMENTS,
-                msg: 'either opts.maxReqsPerPeriod (${opts.maxReqsPerPeriod}) or opts.periodMillis (${opts.periodMillis}) was null/undefined or not a valid number'
+                type: Rate.errors.BAD_ARGUMENTS,
+                msg: `either opts.maxReqsPerPeriod (${opts.maxReqsPerPeriod}) or opts.periodMillis (${opts.periodMillis}) was null/undefined or not a valid number`
             })
         }
         else if (!key) {
-            next({error: Rate.errors.NO_KEY, 'msg': 'no key was available to find the request.'});
+            next({type: Rate.errors.NO_KEY, 'msg': 'no key was available to find the request.'});
         }
         else {
 
             this.client.get(key, (err, result) => {
                 if (err) {
-                    next({error: Rate.errors.REDIS_ERROR});
+                    next({type: Rate.errors.REDIS_ERROR});
                 }
                 else if (result) {
 
@@ -79,7 +80,7 @@ Rate.prototype.limit = function rateLimit(opts) {
 
                         if (diff <= periodMillis) {
                             error = {
-                                error: Rate.errors.RATE_EXCEEDED,
+                                type: Rate.errors.RATE_EXCEEDED,
                                 msg: 'Exceeded ' + maxReqsPerPeriod + ' requests per second for XRE events'
                             };
                         }
@@ -88,7 +89,7 @@ Rate.prototype.limit = function rateLimit(opts) {
                     debug('result array length:', result.length);
                     this.client.set(key, JSON.stringify(result), err => {
                         if (err) {
-                            next({error: Rate.errors.REDIS_ERROR, msg: err});
+                            next({type: Rate.errors.REDIS_ERROR, msg: err});
                         }
                         else {
                             this.client.expire(key, Math.ceil(periodMillis / 1000) + 5); // expire the key at least 5 seconds after
@@ -101,7 +102,7 @@ Rate.prototype.limit = function rateLimit(opts) {
                     debug('setting key for first time.');
                     this.client.set(key, JSON.stringify([Date.now()]), err => {
                         if (err) {
-                            next({error: Rate.errors.REDIS_ERROR, msg: err});
+                            next({type: Rate.errors.REDIS_ERROR, msg: err});
                         }
                         else {
                             this.client.expire(key, Math.ceil(periodMillis / 1000) + 5); // expire the key at least 5 seconds after
