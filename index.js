cd22b6e21bcda0d8c1c726bd1879be78a1070ede
the1mills <alexander.d.mills@gmail.com>
2016-02-07 19:49:06 -0800

    Cleaned up some stuff

--- index.js
+++ index.js
@@ -3,20 +3,28 @@
  */
 
 
-    ///////////////////////////////////////////////
+///////////////////////////////////////////////
 
+var assert = require('assert');
 var Redis = require('ioredis');
 
 ///////////////////////////////////////////////////
 
 /*
-This is the simple version of this module that only rate limits requests by user/request id using Redis
-*/
+ This is the simple version of this module that only rate limits requests by user/request id using Redis
+
+ TODO: need to allow for rate limiting on particular routes
+ e.g., if the user has two different routes for which they want a different rate limit, then the Redis key has to be different
+
+ */
 
 ////////////////////////////////////////////////////
 
 
 function Rate(conf) {
+
+    this.verbose = conf.verbose !== false;  //default to true
+
     if (conf.redis) {
         if (conf.redis.client) {
             this.client = conf.redis.client;
@@ -25,7 +33,7 @@ function Rate(conf) {
             this.client = new Redis(conf.redis);
         }
     }
-    else{
+    else {
         throw new Error('No Redis configuration provided to Curtain module constructor');
     }
 }
@@ -39,24 +47,100 @@ Rate.errors = Rate.prototype.errors = Object.freeze({
 });
 
 
+function skip(req, res, next) {
+    console.log('req with path:', req.path, 'was set to skip....');
+    req.skipCurtain = true;
+    next();
+}
+
+function only(req, res, next) {
+    console.log('req with path:', req.path, 'was set to only....');
+    req.onlyCurtain = true;
+    next();
+}
+
+
 Rate.prototype.limit = function rateLimit(opts) {
 
-    var maxReqsPerPeriod = opts.maxReqsPerPeriod ? parseInt(opts.maxReqsPerPeriod) : null;
-    var periodMillis = opts.periodMillis ? parseInt(opts.periodMillis) : null;
-    var identifier = opts.identifier ? String(opts.identifier) : null;
+    var self = this;
+    var isIncludeOnly = false;
 
+    var router, maxReqsPerPeriod, periodMillis, identifier, excludeRoutes, includeRoutes;
+
+    try {
+        if (opts.app && opts.router) {
+            throw new Error('You passed both opts.app and opts.router, please choose one only.');
+        }
+        router = opts.router || opts.app;
+        maxReqsPerPeriod = opts.maxReqsPerPeriod ? parseInt(opts.maxReqsPerPeriod) : null;
+        periodMillis = opts.periodMillis ? parseInt(opts.periodMillis) : null;
+        identifier = opts.identifier ? String(opts.identifier) : null;
+        excludeRoutes = opts.excludeRoutes ? JSON.parse(JSON.stringify(opts.excludeRoutes)) : null;
+        includeRoutes = opts.includeRoutes ? JSON.parse(JSON.stringify(opts.includeRoutes)) : null;
+
+        if (includeRoutes) {
+            assert(router.use);
+            assert(Array.isArray(includeRoutes));
+        }
+        if (excludeRoutes) {
+            assert(router.use);
+            assert(Array.isArray(excludeRoutes));
+        }
+
+    }
+    catch (err) {
+        return (req, res, next) => {
+            next(err);
+        }
+    }
+
+    (excludeRoutes || []).forEach(function (route) {
+        router.use(route, skip);
+    });
+
+    (includeRoutes || []).forEach(function (route) {
+        isIncludeOnly = true;
+        router.use(route, only);
+    });
 
     return (req, res, next) => {
 
+        if (req.skipCurtain === true) {
+            console.log('req with url:', req.path, 'was *skipped* by curtain.');
+            delete req.skipCurtain; //so that other curtain limiters can work correctly
+            return next();
+        }
+        else {
+            console.log('req with url:', req.path, 'was *processed* by curtain.');
+        }
 
-        function redisSet(error, client, value) {
+        if (isIncludeOnly && !req.onlyCurtain) {
+            console.log('req with url:', req.path, 'was *skipped* by curtain because it was not *included*.');
+            return next();
+        }
 
-            client.set(key, value, err => {
+        delete req.onlyCurtain;  //delete this property so that it doesn't interfere with other curtain limiters
+
+        req.curtained = req.curtained ? req.curtained++ : 1;
+
+        if (req.curtained > 1 && this.verbose) {
+            console.log('Warning: rate limiter used twice for this same request. Two suppress warnings like this, use verbose:false options.');
+        }
+
+
+        function redisSet(error, value) {
+
+            self.client.set(key, value, err => {
                 if (err) {
-                    next({type: Rate.errors.REDIS_ERROR, msg: err});
+                    if(error){
+                        next(error);
+                    }
+                    else{
+                        next({type: Rate.errors.REDIS_ERROR, msg: err});
+                    }
                 }
                 else {
-                    client.expire(key, Math.ceil(periodMillis / 1000) + 5); // expire the key at least 5 seconds after
+                    self.client.expire(key, Math.ceil(periodMillis / 1000) + 5); // expire the key at least 5 seconds after
                     next(error); //if there's an error, send it on
                 }
             });
@@ -64,7 +148,7 @@ Rate.prototype.limit = function rateLimit(opts) {
 
         var key = String(req[identifier]);
 
-        if(!identifier || !key){
+        if (!identifier || !key) {
             next({
                 type: Rate.errors.BAD_ARGUMENTS,
                 msg: `opts.identifier given as (${opts.identifier}) could not produce a valid result from the req object`
@@ -76,7 +160,7 @@ Rate.prototype.limit = function rateLimit(opts) {
                 msg: `opts.periodMillis given as (${opts.periodMillis}) was null/undefined or not a valid number`
             })
         }
-        else if(!periodMillis){
+        else if (!periodMillis) {
             next({
                 type: Rate.errors.BAD_ARGUMENTS,
                 msg: `opts.maxReqsPerPeriod given as (${opts.maxReqsPerPeriod}) was null/undefined or not a valid number`
@@ -112,12 +196,12 @@ Rate.prototype.limit = function rateLimit(opts) {
                         }
                     }
 
-                    redisSet(error, this.client, JSON.stringify(result));
+                    redisSet(error, JSON.stringify(result));
 
                 }
                 else {
                     // setting new value in redis
-                    redisSet(null, this.client, JSON.stringify([Date.now()]));
+                    redisSet(null, JSON.stringify([Date.now()]));
                 }
 
             });
