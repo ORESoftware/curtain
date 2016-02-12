d323dcbe2a01d289b9539853418a41ac5ca29c8b
Alex Mills <alexmillsmail@gmail.com>
2016-02-11 19:27:46 -0800

    Added gulpfile

--- suman.ioc.js
+++ suman.ioc.js
@@ -1,34 +1,56 @@
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { return step("next", value); }, function (err) { return step("throw", err); }); } } return step("next"); }); }; }
+
 /**
  * Created by amills001c on 2/8/16.
  */
 
-
-
 var async = require('async');
 
+function returnsPromise() {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            resolve({ temp: 'A' });
+        }, 1000);
+    });
+}
 
 module.exports = function loadAsyncDepsForSuman(suman, cb) {
 
     suman.configure({
 
-        'request': function (cb) {
+        'request': function () {
 
-            setTimeout(function () {
-                cb(null,
-                    require('request')
-                );
-            }, 1000);
-
+            return require('request');
+            //setTimeout(function () {
+            //    cb(null,
+            //        require('request')
+            //    );
+            //}, 1000);
         },
         'ioredis': function (cb) {
 
             setTimeout(function () {
-                cb(null,
-                    require('ioredis')
-                );
+                cb(null, require('ioredis'));
             }, 1000);
-        }
+        },
+
+        'didj': function () {
+
+            return new Promise(function (resolve) {
+                setTimeout(function () {
+                    resolve({ temp: 'A' });
+                }, 1000);
+            });
+        },
+        'mark': function () {
+            var ref = _asyncToGenerator(function* () {
+                return yield returnsPromise();
+            });
+
+            return function mark() {
+                return ref.apply(this, arguments);
+            };
+        }()
 
     });
-
 };
